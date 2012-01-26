2f0d32c1f7dd74f90692e48def7d6863da5f0e21
Gus Michel <gus3@michelfamily.org>
2012-01-26 17:43:41 -0600

    uploading mostly framework code

---
+++ src/com/slidellrobotics/tigerrobotics/highshot/OI.java
@@ -0,0 +1,72 @@
+
+package com.slidellrobotics.tigerrobotics.highshot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+    private Joystick leftJoystick;
+    private Joystick rightJoystick;
+    private Button fireBall;
+    private Button shiftGear;
+    
+    public OI() {
+        leftJoystick = new Joystick(RobotMap.leftJoystick);
+        rightJoystick = new Joystick(RobotMap.rightJoystick);
+        fireBall = new JoystickButton(rightJoystick, RobotMap.fireButton);
+        shiftGear = new JoystickButton(leftJoystick, RobotMap.gearShiftButton);
+    }
+    
+    public Joystick getLeftJoystick() {
+        return leftJoystick;
+    }
+    
+    public Joystick getRightJoystick() {
+        return rightJoystick;
+    }
+    
+    public Button getFireButton() {
+        return fireBall;
+    }
+    
+    public Button getGearShift() {
+        return shiftGear;
+    }
+    //// CREATING BUTTONS
+    // One type of button is a joystick button which is any button on a joystick.
+    // You create one by telling it which joystick it's on and which button
+    // number it is.
+    // Joystick stick = new Joystick(port);
+    // Button button = new JoystickButton(stick, buttonNumber);
+    
+    // Another type of button you can create is a DigitalIOButton, which is
+    // a button or switch hooked up to the cypress module. These are useful if
+    // you want to build a customized operator interface.
+    // Button button = new DigitalIOButton(1);
+    
+    // There are a few additional built in buttons you can use. Additionally,
+    // by subclassing Button you can create custom triggers and bind those to
+    // commands the same as any other Button.
+    
+    //// TRIGGERING COMMANDS WITH BUTTONS
+    // Once you have a button, it's trivial to bind it to a button in one of
+    // three ways:
+    
+    // Start the command when the button is pressed and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenPressed(new ExampleCommand());
+    
+    // Run the command while the button is being held down and interrupt it once
+    // the button is released.
+    // button.whileHeld(new ExampleCommand());
+    
+    // Start the command when the button is released  and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenReleased(new ExampleCommand());
+}
+
---
+++ src/com/slidellrobotics/tigerrobotics/highshot/RobotMap.java
@@ -0,0 +1,27 @@
+package com.slidellrobotics.tigerrobotics.highshot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static final int leftMotor = 1;
+    // public static final int rightMotor = 2;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static final int rangefinderPort = 1;
+    // public static final int rangefinderModule = 1;
+    public static final int leftMotor = 1;
+    public static final int rightMotor = 2;
+    public static final int leftJoystick = 2;
+    public static final int rightJoystick = 1;
+    public static final int gearSolinoid = 1;
+    public static final int ballFeedMotor = 3;
+    public static final int fireButton = 1;
+    public static final int gearShiftButton = 1;
+}
